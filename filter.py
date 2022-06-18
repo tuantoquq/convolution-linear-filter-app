82642fae5dfce1df28ad422179520291feb1a3a5
wind123mountain <57493825+wind123mountain@users.noreply.github.com>
2022-06-18 10:35:02 +0700

    update EX_Ch3

--- filter.py
+++ filter.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 def gray_img(path):
-    img_src = cv2.imread(cv2.samples.findFile("test.jpg"))
+    img_src = cv2.imread(cv2.samples.findFile(path))
     img_src = cv2.equalizeHist(cv2.cvtColor(img_src, cv2.COLOR_BGR2GRAY))
 
     return img_src
@@ -23,18 +23,18 @@ def hight_pass_filter(img):
     return img_res
 
 def horizontal_edge_detect(img):
-    kernel_horizontal_edge = np.array([[1, 0, -1.0], 
-                                [2.0, 0, -2.0],
-                                [1.0, 0, -1.0]])
+    kernel_horizontal_edge = np.array([[1, 2, 1],
+                                [0, 0, 0],
+                                [-1, -2, -1]])
 
     img_horizontal_edge = cv2.filter2D(img,-1,kernel_horizontal_edge)
 
     return img_horizontal_edge
 
 def vertical_edge_detect(img):
-    kernel_vertical_edge = np.array([[1, 2, 1],
-                                [0, 0, 0],
-                                [-1, -2, -1]])
+    kernel_vertical_edge = np.array([[1, 0, -1.0], 
+                                [2.0, 0, -2.0],
+                                [1.0, 0, -1.0]])
 
     img_vertical_edge = cv2.filter2D(img,-1,kernel_vertical_edge)
 
